d9b210f5b7d1b5fb6a956d69f2127c1faccdadb4
Raul <raul.rusu@tracotsetgo.com>
2023-11-14 17:41:32 +0200

    feat: add incomplete header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
+<script setup>
+  import MainHeader from "./components/MainHeader.vue"
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <MainHeader/>
+    <router-view/>
+ 
 </template>
 
 <style>
@@ -13,10 +15,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
 }
 
 nav a {
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="elevation-10">
+    <div class="flex-fill mx-10">
+      <div class="d-flex justify-space-between align-center" size="large">
+        <div class="text-h4">AdoptMe: {{ "More info" }}</div>
+        <v-btn rounded flat color="#ffc974" text="Shelter"></v-btn>
+      </div>
+
+      <div class="text-subtitle-1 text-left">Find Your Fur-ever Companion!</div>
+    </div>
+  </header>
+</template>
+
+<style>
+header {
+  display: flex;
+  min-height: 5rem;
+  padding: 0.8rem;
+  background: rgb(255, 201, 116);
+  background: linear-gradient(
+    0deg,
+    rgba(255, 201, 116, 0.75) 0%,
+    rgba(255, 255, 255, 0) 50%
+  );
+}
+</style>
+
+<script></script>
