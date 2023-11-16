383e4bd912337f36c50830c8059c0e6ae782db08
Raul <raul.rusu@tracotsetgo.com>
2023-11-16 18:21:18 +0200

    WIP: main shelter page

---
+++ src/components/PetCards/ShelterPetCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="mx-auto" variant="elevated">
+    <div class="pa-2 card-background">
+      <div class="d-flex">
+        <v-img
+          :aspect-ratio="16 / 9"
+          cover
+          src="https://transcode-v2.app.engoo.com/image/fetch/f_auto,c_lfill,w_300,dpr_3/https://assets.app.engoo.com/images/QyDHB4YHkK2V6TA6QkDzSIMbQpg9IIUKO5tn8KuDcJ1.jpeg"
+        />
+        <div class="mx-4 align-self-center text-h5">{{name}}</div>
+      </div>
+
+      <div class="d-flex flex-column align-start mt-2">
+        <div class="text-subtitle-1">Gender: {{gender}}</div>
+        <div class="text-subtitle-1">Date of birth: {{dateOfBirth}}</div>
+      </div>
+
+      <div class="d-flex flex-row justify-space-between">
+        <div class="d-inline">
+          <div class="d-inline">d</div>
+          <div class="d-inline">a</div>
+        </div>
+
+        <div class="text-decoration-underline font-italic" :on-click="() => {moreCallback()}">More→</div>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<style scoped>
+.card-background {
+  background: rgb(42, 157, 143);
+  background: linear-gradient(
+    0deg,
+    rgba(42, 157, 143, 0.4) 0%,
+    rgba(42, 157, 143, 0) 100%
+  );
+}
+
+.text-subtitle-1 {
+  line-height: 1.25rem;
+}
+</style>
+
+<script>
+    export default {
+        props: ['name', 'gender', 'dateOfBirth', 'moreCallback'],
+        name: 'ShelterPetCard'
+    }
+</script>
--- src/views/Shelter/ShelterMainView.vue
+++ src/views/Shelter/ShelterMainView.vue
@@ -1,48 +1,88 @@
+<script setup>
+import ShelterPetCard from "@/components/PetCards/ShelterPetCard.vue";
+
+import { ref } from "vue";
+
+// state
+const shelterAnimals = ref([
+  { name: "aaaaa", dateOfBirth: "12.12.2021", gender: "male" },
+  { name: "bbbbb", dateOfBirth: "12.12.2020", gender: "male" },
+  { name: "ccccc", dateOfBirth: "12.12.2019", gender: "female" },
+  { name: "ddddd", dateOfBirth: "12.12.2018", gender: "male" },
+]);
+</script>
+
 <template>
   <div class="ma-8">
-    <div class="d-flex justify-space-between">
-      <div class="d-inline">
-        <div class="d-inline">this is a serachbar</div>
-        <div class="d-inline ml-2">add</div>
+    <div class="d-flex justify-space-between" color="green">
+      <div class="d-flex flex-fill">
+        <v-text-field
+          flat
+          class="search-bar mr-4"
+          :loading="loading"
+          density="compact"
+          variant="solo"
+          label="Search"
+          single-line
+          hide-details
+          append-inner-icon="mdi-magnify"
+          bg-color="rgba(255, 201, 116, 0.7)"
+        ></v-text-field>
+
+        <v-btn flat height="auto" color="rgb(255, 201, 116, 0.6)">Add +</v-btn>
       </div>
-      <div class="d-inline">filter</div>
+
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn
+            flat
+            color="rgb(255, 201, 116, 0.6)"
+            v-bind="props"
+            height="auto"
+          >
+            Filter &nbsp<font-awesome-icon
+              :icon="['fas', 'arrow-down']"
+              style="color: #5bb4a9"
+            />
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </div>
 
-    <v-container>
-      <v-row align="center" justify="start">
-        <v-col cols="4">
-          <v-card class="mx-auto" variant="elevated">
-            <div class="pa-2 card-background">
-              <div class="d-flex">
-                <v-img
-                  cover
-                  src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
-                />
-                <div class="mx-4 align-self-center">Pisciuta</div>
-              </div>
-
-              <div class="d-flex flex-column align-start">
-                <div>Gender: Female</div>
-                <div>Date of birth: 18.18.2020</div>
-              </div>
-            </div>
-          </v-card>
+    <v-container fluid>
+      <v-row justify="start">
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="4"
+          v-for="(pet, index) in shelterAnimals"
+          v-bind:key="index"
+        >
+          <ShelterPetCard
+            :name="pet.name"
+            :dateOfBirth="pet.dateOfBirth"
+            :gender="pet.gender"
+          ></ShelterPetCard>
         </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
 <style>
-.card-background {
-  background: rgb(42, 157, 143);
-  background: linear-gradient(
-    0deg,
-    rgba(42, 157, 143, 0.4) 0%,
-    rgba(42, 157, 143, 0) 100%
-  );
+.search-bar {
+  max-width: 30%;
 }
 </style>
-
 <script>
 export default {
   name: "ShelterMainView",
