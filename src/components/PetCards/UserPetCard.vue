04726b250f775b592bc1669f165913998f18dccf
tabita <91674360+914LucaciuTabita@users.noreply.github.com>
2023-11-18 13:57:17 +0200

    done for now

--- src/components/PetCards/UserPetCard.vue
+++ src/components/PetCards/UserPetCard.vue
@@ -1,39 +1,50 @@
 <template>
-  <v-card class="mx-auto" variant="elevated">
-    <div class="pa-2 card-background">
+  <v-card class="mx-auto" variant="flat">
+    <div class="card-background">
+      
       <div class="d-flex">
         <v-img
-          class="pet-image"
+          :aspect-ratio="16 / 9"
           cover
           :src="imageUrl"
-        />
-        <div class="mx-4 align-self-center">
-          <div class="animal-name-text text-h6">{{ name }}</div>
-          <div class="gender-text text-subtitle-1">-{{ gender }}-</div>
-        </div>
+        >
+        </v-img>
       </div>
 
-      <div class="d-flex flex-column align-start">
-        <div class="text-subtitle-1">Shelter: Whisker Whispers</div>
+      <div class="card-info-background pa-2">
+        <div>
+          <div class="d-flex flex-column align-center">
+            <div class="mx-4 align-self-center text-h5">{{ name }}</div>
+            <div class="text-subtitle-1">Gender: {{ gender }}</div>
+            <div class="text-subtitle-1">Shelter: {{ shelterName }}</div>
+          </div>
+        </div>
+
+        <div class="d-flex flex-column align-end">
+            <v-btn
+              variant="plain"
+              class="more-button text-body-1 align-end" 
+              text="More"
+              append-icon="mdi-arrow-right"
+            >
+              More
+            </v-btn>
+          </div>
+
       </div>
-      <div class="d-flex flex-column align-end more-text text-subtitle-1" @click="moreCallback">More→</div>
     </div>
   </v-card>
 </template>
 
 <style scoped>
 .card-background {
-  background: rgb(42, 157, 143);
-  background: linear-gradient(
-    0deg,
-    rgba(42, 157, 143, 0.4) 0%,
-    rgba(42, 157, 143, 0) 100%
-  );
+  border: 2px solid rgba(42, 157, 143, 0.2);
 }
 
-.pet-image {
-  height: 20rem; /* Adjust the height as needed */
-  object-fit: cover;
+.card-info-background {
+  background: rgba(42, 157, 143, 0.1);
+  /*border: 2px solid rgba(42, 157, 143, 0.2);*/
+  border-top: 0;
 }
 
 .text-subtitle-1 {
@@ -46,14 +57,11 @@
   font-style: italic;
 }
 
-.more-text {
-  text-decoration: underline;
-}
 </style>
 
 <script>
-    export default {
-        props: ['name', 'gender', 'imageUrl', 'moreCallback'],
-        name: 'UserPetCard'
-    }
+export default {
+  props: ["name", "gender", "shelterName", "imageUrl", "moreCallback"],
+  name: "UserPetCard",
+};
 </script>
--- src/views/User/AnimalsListView.vue
+++ src/views/User/AnimalsListView.vue
@@ -3,11 +3,11 @@ import UserPetCard from "@/components/PetCards/UserPetCard.vue";
 import { ref, watch } from "vue";
 
 const shelterAnimals = ref([
-  { name: "Rocky", gender: "male", imageUrl: "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J", shelter: "Whisker Whispers" },
-  { name: "Max", gender: "male", imageUrl: "https://images.rawpixel.com/image_300/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8xNV9waG90b19vZl9hX2RvZ19ydW5uaW5nX3dpdGhfb3duZXJfYXRfcGFya19lcF9mM2I3MDQyZC0zNWJlLTRlMTQtOGZhNy1kY2Q2OWQ1YzQzZjlfMi5qcGc.jpg", shelter: "Purr Paradise" },
-  { name: "Chloe", gender: "female", imageUrl: "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg", shelter: "Meow Mansion" },
-  { name: "Oliver", gender: "male", imageUrl: "https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg", shelter: "Paw Palace" },
-  { name: "Felix", gender: "male", imageUrl: "https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.668xw:1.00xh;0.119xw,0&resize=1200:*", shelter: "Whisker Whispers" },
+  { name: "Rocky", gender: "male", shelterName: "Whisker Whispers", imageUrl: "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J", shelter: "Whisker Whispers" },
+  { name: "Max", gender: "male", shelterName: "Whisker Whispers", imageUrl: "https://images.rawpixel.com/image_300/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8xNV9waG90b19vZl9hX2RvZ19ydW5uaW5nX3dpdGhfb3duZXJfYXRfcGFya19lcF9mM2I3MDQyZC0zNWJlLTRlMTQtOGZhNy1kY2Q2OWQ1YzQzZjlfMi5qcGc.jpg", shelter: "Purr Paradise" },
+  { name: "Chloe", gender: "female", shelterName: "Paws and Claws Haven", imageUrl: "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg", shelter: "Meow Mansion" },
+  { name: "Oliver", gender: "male", shelterName: "Paws and Claws Haven", imageUrl: "https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg", shelter: "Paw Palace" },
+  { name: "Felix", gender: "male", shelterName: "Whisker Whispers", imageUrl: "https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.668xw:1.00xh;0.119xw,0&resize=1200:*", shelter: "Whisker Whispers" },
 ]);
 
 const searchTerm = ref('');
@@ -45,7 +45,7 @@ const openDetails = (pet) => {
         <v-text-field
           v-model="searchTerm"
           flat
-          class="search-bar"
+          class="search-bar mr-4"
           :loading="loading"
           density="compact"
           variant="solo"
@@ -79,7 +79,9 @@ const openDetails = (pet) => {
       </v-menu>
     </div>
 
-    <v-container fluid>
+    <v-divider class="my-3" thickness="2" color="rgba(42, 157, 143, 0.8)"></v-divider>
+    
+    <v-container fluid class="pa-0">
       <v-row justify="start">
         <v-col
           cols="12"
@@ -92,6 +94,7 @@ const openDetails = (pet) => {
           <UserPetCard
             :name="pet.name"
             :gender="pet.gender"
+            :shelterName="pet.shelterName"
             :imageUrl="pet.imageUrl"
             :moreCallback="() => openDetails(pet)"
           ></UserPetCard>
@@ -103,8 +106,7 @@ const openDetails = (pet) => {
 
 <style>
 .search-bar {
-  min-width: 20%;
-  max-width: 25%;
+  max-width: 30%;
 }
 
 .card-background {
