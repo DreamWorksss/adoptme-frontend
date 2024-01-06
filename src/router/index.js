396f65375abbacff3f36ce4fd7e0fe2fdd2c63d3
DARIA-MARIA MATEI <daria.maria.matei@stud.ubbcluj.ro>
2024-01-06 16:34:22 +0200

    add adopt at distance form

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import PetDetailsPage from "../views/User/PetDetailsPage.vue";
 import ShelterMainView from '../views/Shelter/ShelterMainView.vue'
 import PetListView from '../views/User/PetListView.vue'
 import ShelterPetDetailsPage from '../views/Shelter/ShelterPetDetailsPage.vue'
+import AdoptAtDistanceFormPage from "@/views/User/AdoptAtDistanceFormPage.vue";
 
 const routes = [
   {
@@ -37,6 +38,11 @@ const routes = [
     path: '/shelter/pet/:id',
     name: 'ShelterPetDetails',
     component: ShelterPetDetailsPage
+  },
+  {
+    path: '/adopt-at-distance/:id',
+    name: 'AdoptAtDistance',
+    component: AdoptAtDistanceFormPage
   }
 ]
 
